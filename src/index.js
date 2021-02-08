30508aea0aa6c81ae372255dd0b037c6132f41a0
Shinoda <d1maofficial666@gmail.com>
2021-02-08 23:31:36 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if (n >= 0) {
+    return Number(String(n).split('').reverse().join(''));
+  } else {
+    return Number(String(Math.abs(n)).split('').reverse().join(''));
+  }
 }
